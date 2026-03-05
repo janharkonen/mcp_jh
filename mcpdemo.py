670f8b3d2fdc59c6d533cbdd468283889bccb3a8
Jan Härkönen <jan.harkonen@gmail.com>
2026-03-06 01:04:24 +0200

    add mystery function

--- mcpdemo.py
+++ mcpdemo.py
@@ -7,5 +7,10 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+@mcp.tool
+def mystery_function(a: int, b: int) -> int:
+    """Get a mysterious result"""
+    return a * a - 2 * b
+
 if __name__ == "__main__":
     mcp.run()
